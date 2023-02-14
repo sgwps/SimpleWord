a72d1259070faed144eaddfc824d740f3b97b7f5
Grigory Borisenko <7010504@gmail.com>
2023-02-14 14:36:38 +0300

    html added. wip

--- SimpleWordAPI/Controllers/NewCollection.cs
+++ SimpleWordAPI/Controllers/NewCollection.cs
@@ -1,18 +1,35 @@
 using Microsoft.AspNetCore.Mvc;
-
+using System;
 namespace SimpleWordAPI.Controllers;
 
 [ApiController]
-[Route("[controller]")]
-public class NewCollectionController : ControllerBase
+[Route("new_collection")]
+public class NewCollectionGetController : ControllerBase
 {
     public ContentResult Get(){
-        throw new NotImplementedException();
+        return Content(System.IO.File.ReadAllText(@"TestViews/Input.HTML", System.Text.Encoding.UTF8), "text/html");
     }
 
+    /*
     public StatusCodeResult Post(string collection){  //пропихнуть json
         throw new NotImplementedException();
     }
+    */
+
+
+}
+
+
+[ApiController]
+[Route("new_collection_post")]
+public class NewCollectionPostController : ControllerBase
+{
+    public StatusCodeResult Post(){  //пропихнуть json
+        Console.WriteLine("aaaaa");
+        //Console.WriteLine(collection);
+        return StatusCode(200);
+    }
+
 
 
 }
